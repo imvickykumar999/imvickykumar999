4d44937d6f93944e440704bdf39c7cb6a006a286
imvickykumar999 <imvickykumar999@gmail.com>
2021-07-25 10:52:03 +0530

    mailed covid

--- vicks/covidmail.py
+++ vicks/covidmail.py
@@ -21,45 +21,45 @@ def covail(l=[
          ).to_excel(writer, sheet_name = f'Sheet {i}')
     writer.save()
 
-    # import smtplib
-    # from email.mime.multipart import MIMEMultipart
-    # from email.mime.text import MIMEText
-    # from email.mime.base import MIMEBase
-    # from email import encoders
-    #
-    # fromaddr = "imvickykumar999@gmail.com"
-    # # toaddr = "hellovickykumar123@gmail.com"
-    #
-    # msg = MIMEMultipart()
-    # msg['From'] = fromaddr
-    # msg['To'] = toaddr
-    #
-    # msg['Subject'] = "COVID19 Slot Notification"
-    # body = f'''
-    #
-    # Book your Slot here : https://selfregistration.cowin.gov.in/
-    #
-    # This E-Mail is Sent using python code by vicks,
-    # Slots is... (open attached excel file)
-    #
-    # {l}
-    # '''
-    # msg.attach(MIMEText(body, 'plain'))
-    # attachment = open(filename, "rb")
-    #
-    # p = MIMEBase('application', 'octet-stream')
-    # p.set_payload((attachment).read())
-    # encoders.encode_base64(p)
-    # p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-    # msg.attach(p)
-    #
-    # s = smtplib.SMTP('smtp.gmail.com', 587)
-    # s.starttls()
-    # s.login(fromaddr, "heyvix999@")
-    #
-    # text = msg.as_string()
-    # s.sendmail(fromaddr, toaddr, text)
-    # s.quit()
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+    from email.mime.base import MIMEBase
+    from email import encoders
+
+    fromaddr = "imvickykumar999@gmail.com"
+    # toaddr = "hellovickykumar123@gmail.com"
+
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+
+    msg['Subject'] = "COVID19 Slot Notification"
+    body = f'''
+
+    Book your Slot here : https://selfregistration.cowin.gov.in/
+
+    This E-Mail is Sent using python code by vicks,
+    Slots is... (open attached excel file)
+
+    {l}
+    '''
+    msg.attach(MIMEText(body, 'plain'))
+    attachment = open(filename, "rb")
+
+    p = MIMEBase('application', 'octet-stream')
+    p.set_payload((attachment).read())
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    msg.attach(p)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(fromaddr, "heyvix999@")
+
+    text = msg.as_string()
+    s.sendmail(fromaddr, toaddr, text)
+    s.quit()
 
     return len(l)
 
