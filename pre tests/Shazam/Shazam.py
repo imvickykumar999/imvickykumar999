cd9ef927577019ab49edd64a29271d923bd9de86
imvickykumar999 <imvickykumar999@gmail.com>
2022-01-03 22:45:21 +0530

    pre test folder moved to local project folder

--- pre tests/Shazam/Shazam.py
+++
@@ -1,8 +0,0 @@
-from ShazamAPI import Shazam
-
-mp3_file_content_to_recognize = open('Stebin Ben - Baarish Ban Jaana_192(PagalWorld.com.se).mp3', 'rb').read()
-
-shazam = Shazam(mp3_file_content_to_recognize)
-recognize_generator = shazam.recognizeSong()
-while True:
-    print(next(recognize_generator)) # current offset & shazam response to recognize requests
--- pre tests/reels/exe.py
+++
@@ -1,4 +0,0 @@
-import os, shutil
-
-# os.system('pip install pyinstaller')
-os.system('pyinstaller --onefile reels.py')
