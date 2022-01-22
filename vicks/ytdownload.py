36fbedd72a2faecc79bb07a4cd25c1bba10e9500
imvickykumar999 <imvickykumar999@gmail.com>
2022-01-22 16:35:27 +0530

    insta

--- vicks/ytdownload.py
+++ vicks/ytdownload.py
@@ -69,3 +69,27 @@ def yt_audio(vid = 'KBtk5FUeJbk', ts=60, te=600):
         print(e)
 
     return vid
+
+
+def download_yt(vid = '_iXg6mEbE28'):
+    from pytube import YouTube
+
+    #ask for the link from user
+    # link = input("Enter the link of YouTube video you want to download:  ")
+    link = f'https://www.youtube.com/watch?v={vid}'
+    yt = YouTube(link)
+
+    #Showing details
+    print("Title: ",yt.title)
+    print("Number of views: ",yt.views)
+    print("Length of video: ",yt.length)
+    print("Rating of video: ",yt.rating)
+    #Getting the highest resolution possible
+    ys = yt.streams.get_highest_resolution()
+
+    #Starting download
+    print("Downloading...")
+    ys.download()
+    
+    print("Download completed!!")
+    return vid
